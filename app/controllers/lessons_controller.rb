55e47686fb0f61e22da4c5befe6b48760a76948f
Laura Brooks <laura.brooks0@outlook.com>
2022-01-25 21:11:33 +0900

    added autorize to methods in lesson controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,27 +3,38 @@ class LessonsController < ApplicationController
 
   def index
     @lessons = Lesson.all
+    @lessons = policy_scope(Lesson)
   end
 
   def show
+    lesson_policy_authorize
   end
 
   def new
+    lesson_policy_authorize
   end
 
   def edit
+    lesson_policy_authorize
   end
 
   def create
+    lesson_policy_authorize
   end
 
   def update
+    lesson_policy_authorize
   end
 
   def destroy
+    lesson_policy_authorize
   end
 
   private
+
+  def lesson_policy_authorize
+    authorize @lesson
+  end
 
   def set_lesson
     @lesson = Lesson.find(params[:id])
