4b744238abf80f2d12f8979cae204781437cfe8b
Laura Brooks <laura.brooks0@outlook.com>
2022-01-26 16:56:14 +0900

    booking model references added

--- db/migrate/20220126075406_create_bookings.rb
+++ db/migrate/20220126075406_create_bookings.rb
@@ -1,6 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :lesson, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_090408) do
+ActiveRecord::Schema.define(version: 2022_01_26_075406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "lesson_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lesson_id"], name: "index_bookings_on_lesson_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.string "skill_name"
@@ -49,5 +58,7 @@ ActiveRecord::Schema.define(version: 2022_01_22_090408) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "lessons"
+  add_foreign_key "bookings", "users"
   add_foreign_key "lessons", "users"
 end
