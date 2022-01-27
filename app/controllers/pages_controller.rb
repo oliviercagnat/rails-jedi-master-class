b6503fd3142a68b374dde60dc70ee29b9bb9b59d
Laetitia Taddei <laetdi@gmail.com>
2022-01-27 21:30:11 +0900

    just a little pimping

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
     @lessons = if current_user.side == "Light"
-Lesson.includes(:user).where(user: { side: "Light" }).first(3)
+Lesson.includes(:user).where(user: { side: "Light" }).first(5)
 else
-Lesson.includes(:user).where(user: { side: "Dark" }).first(3)
+Lesson.includes(:user).where(user: { side: "Dark" }).first(5)
 end
   end
 end
