be3506870730a2b1ece25da5774976af2604284c
Olivier Cagnat <oliviercagnat@gmail.com>
2022-01-29 12:57:21 +0900

    upload picture, new migration files, add logout button

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'pundit'
 
 gem 'devise'
 gem 'faker'
+gem 'cloudinary', '~> 1.16.0'
 
 group :development, :test do
   gem 'pry-byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,15 +26,19 @@ puts "Finished!"
 
 puts "Creating lessons..."
 
+
 10.times do
+  unsplash_url = "http://source.unsplash.com/1600x900/?superpower"
+  skill_name = Faker::Superhero.power
   start_at = Date.current + rand(10).days
   lesson = Lesson.create!(
     user: User.all.sample,
-    skill_name: Faker::Superhero.power,
+    skill_name: skill_name,
     description: Faker::Lorem.paragraph(sentence_count: 30, supplemental: false, random_sentences_to_add: 6),
     start_at: start_at,
     end_at: start_at + rand(1..3).days,
-    price: rand(20_000..40_000)
+    price: rand(20_000..40_000),
+    skill_picture: "#{unsplash_url}"
   )
   puts "Lesson #{lesson.id} is created"
 end
