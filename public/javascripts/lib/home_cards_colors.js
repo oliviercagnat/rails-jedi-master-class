795777f55341cb57a6d306317a63e8c17a543bf4
Laura Brooks <laura.brooks0@outlook.com>
2022-02-05 16:28:02 +0900

    fixed cards on index and home page

--- public/javascripts/lib/home_cards_colors.js
+++
@@ -1,9 +0,0 @@
-var homeCards = document.querySelectorAll('.lesson-card')
-homeCards.forEach(card => {
-  let status = document.getElementById('status').innerText;
-  if (status.includes("Sith")) {
-    card.classList.add("sith")
-  } else {
-    card.classList.add("jedi");
-  };
-});
--- public/javascripts/lib/index_cards_colors.js
+++
@@ -1,9 +0,0 @@
-var indexCards = document.querySelectorAll('.lesson-card')
-indexCards.forEach(card => {
-  let status = card.children[1].children[1].innerText;
-  if (status.includes("Sith")) {
-    card.classList.add("sith")
-  } else {
-    card.classList.add("jedi");
-  }
-});
